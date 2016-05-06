78d359e448ff635561daf91f95ba24320a70f8c5
Rubén Soler <r.solerginer@gmail.com>
2016-05-06 17:37:06 +0200

    mejoras en arquitectura RESTful

--- src/java/com/apiTienda/rest/ApiProductos.java
+++ src/java/com/apiTienda/rest/ApiProductos.java
@@ -3,63 +3,36 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.apiTienda.servlets;
+package com.apiTienda.rest;
 
 import com.apiTienda.javaClass.Producto;
 import com.apiTienda.javaClass.Review;
 import com.apiTienda.javaClass.TransformadorJson;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 /**
  *
  * @author ruben
  */
-public class ServletProductos extends HttpServlet {
+@Path("/productos")
+public class ApiProductos {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param req servlet request
-     * @param res servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest req, HttpServletResponse res)
-            throws ServletException, IOException {
-
-    }
-
-    // <editor-fold defaon if a servlultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws javax.servlet.ServletException
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("application/json;charset=UTF-8");
-        response.setHeader("Access-Control-Allow-Origin:","*");
+    @GET
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+    public String obtenerTodos(){
         Connection con;
         TransformadorJson TJson = new TransformadorJson();
         ArrayList<Producto> listaProductos = new ArrayList<>();
         try {
-            response.setContentType("application/json;charset=UTF-8");
             con = DriverManager.getConnection("jdbc:mysql://localhost:3306/tiendaAngularJS", "root", "root");
             if (con != null) {
 
@@ -108,41 +81,12 @@ public class ServletProductos extends HttpServlet {
 
                     // fin obtener productos
                 }
-                try (PrintWriter out = response.getWriter()) {
-                    out.printf(TJson.toJson(listaProductos.toArray()));
-                }
+
             }
         } catch (SQLException ex) {
             System.out.print(ex);
         }
-
-        processRequest(request, response);
+        return TJson.toJson(listaProductos.toArray());
     }
-
-    
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
